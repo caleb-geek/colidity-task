a214c625a05e2ea4b614fe4e6f7d2e1120e7ad19
caleb <caleb.geek.gk@gmail.com>
2020-07-15 12:34:02 +0300

    change

--- bashscript.sh
+++ bashscript.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+NUMBERS="2 41 -15"
 
-#Declare a simple function
-function solution()
-{
-for f in *.
-do
-	mv ./* ./music
-    echo "sucesseful"
+sum=0
+for NUM in ${NUMBERS}
+do    
+    
+    ((counter++))
+    (( sum += ${NUM#-}))
+  
 done
-}
-#Call the function to print a simple message
-solution
+if ['$sum' -gt '10^18']
+  then
+     echo "Error"
+else
+     echo $sum
+fi
+
+
 
